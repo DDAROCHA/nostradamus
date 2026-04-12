e378f217846f5b2c4f4b9b725d8f0b06bb733fa0
Diego Da Rocha <ddarocha00@gmail.com>
2026-04-12 14:28:04 -0300

    init-03

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'https://tu-backend.onrender.com';
+//const API_URL = 'https://tu-backend.onrender.com';
+const API_URL = 'http://localhost:10000/api';
 
 export const getPrediction = prompt => {
-  return axios.post(`${API_URL}/predict`, { prompt });
+  return axios.post(`${API_URL}/nostradamus`, { prompt });
 };
